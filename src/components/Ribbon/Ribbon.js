39eee25add154eec27595c2e7a2fdf7901a385d0
Komang Luce <komangwluce@gmail.com>
2017-09-25 13:15:22 -0700

    fixed sctoll position jump

--- src/components/Ribbon/Ribbon.js
+++ src/components/Ribbon/Ribbon.js
@@ -5,13 +5,21 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 export default class Ribbon extends Component {
+  
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0)
+    }
+  }
+
   render() {
       const { location : { pathname } } = this.props;
+
       return (
         <div className={`ribbon ${pathname !== '/' ? 'relative' : null}`}>
           <nav>
             <div className='left'>
-              <div className='image'/>
+              <Link to="/"><div className='image'/></Link>
             </div>
             <div className='right'>
               <Link to="/">Home</Link>
