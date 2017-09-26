9dd3799c87b21f192922b65b400cb87510bf4e91
Komang Luce <komangwluce@gmail.com>
2017-09-25 19:29:13 -0700

    fix zoom

--- src/components/Zoom/Zoom.js
+++ src/components/Zoom/Zoom.js
@@ -12,12 +12,9 @@ export default class Zoom extends Component {
       const { renderField, props: { match : { params } } } = this,
         project = projects.find((p) => p.name.split(' ').join('') === params.projectName);
 
-      console.log(project.id === 0 || !!(project.id && !(project.id%2)))
-
       return (
         <div className='zoom'>
-          <div className={`project ${project.id === 0 || !!(project.id && !(project.id%2)) ? 'even' : ''}`}>
-            <div className='image' style={{backgroundImage: `url(${project.imageSrc})`}}/>
+          <div className='project'>
             <div className='text'>
               <h1>{project.name}</h1>
               <p>{project.description}</p>
