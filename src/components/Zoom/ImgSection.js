52ac14026edaca29be76a1ab761340c4f7662661
Komang Luce <komangwluce@gmail.com>
2017-09-26 14:58:58 -0700

    work

--- src/components/Zoom/ImgSection.js
+++ src/components/Zoom/ImgSection.js
@@ -6,14 +6,20 @@ import React, { Component } from 'react';
 
 export default class ImgSection extends Component {
   render() {
-      const { field } = this.props;
-      const isSingle = field.imageSrc.length === 1;
+      const { field } = this.props,
+        isSingle = field.imageSrc.length === 1,
+        width = field.imageSrc.length * window.innerWidth;
 
       return (
-        <div className={`carosel ${isSingle ? '' : 'move'}`} >
+        <div className={`carosel ${isSingle ? '' : 'move'}`} style={{width: width}}>
           {
             field.imageSrc.map((s) => {
-              return <div className='image' style={{backgroundImage: `url(${s})`}}/>
+              return <div className='image' style={
+                {
+                  backgroundImage: `url(${s})`, 
+                  width: window.innerWidth
+                }
+              } />
             })
           }
         </div>
