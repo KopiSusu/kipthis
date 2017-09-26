5f12f0bd1ae5de699425041bc5ac6a02abedf6db
Komang Luce <komangwluce@gmail.com>
2017-09-25 19:42:27 -0700

    fix for svg

--- src/components/Landing/Logos.js
+++ src/components/Landing/Logos.js
@@ -2,26 +2,25 @@
 /* eslint global-require: 0 */
 
 import React, { Component } from 'react';
-
-const logos = [
-  '//witness.org/wp-content/uploads/2014/04/fastCompany-721x227.png',
-  '//s3.amazonaws.com/build-inauth/wp-content/uploads/2017/02/03150910/transparent-paymentssource-logo-01-400x250.png',
-  '//3.bp.blogspot.com/-UgWxBnw1RFY/VEAsR68cN7I/AAAAAAAAFqE/SMt0xbvyaC0/s1600/Logo%2BTime_Magazine.png',
-  '//unify.id/images/venturebeat_logo.png',
-  '//www.stickpng.com/assets/images/58482332cef1014c0b5e49be.png'
-]
+import Fastcompany from './../../theme/svgs/Fastcompany';
+import Paymentsource from './../../theme/svgs/Paymentsource';
+import Time from './../../theme/svgs/Time';
+import Venturebeat from './../../theme/svgs/Venturebeat';
+import Wallstreet from './../../theme/svgs/Wallstreet';
 
 export default class Logos extends Component {
   render() {
       return (
         <div className='logos'>
           <h1>Featured in</h1>
-          {
-            logos.map((l) => (
-              <div className='image' style={{backgroundImage: `url(${l})`}}/>
-            ))
-          }
+          <Fastcompany/>
+          <Paymentsource/>
+          <Time/>
+          <Venturebeat/>
+          <Wallstreet/>
         </div>
       );
   }
 }
+
+
